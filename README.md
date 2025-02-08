0999b8af184b745d0975cf6477bf63fcee21fe56
jiangjunfeng98 <510879374@qq.com>
2025-02-08 05:32:26 +0000

    绑定域名

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 ## 中国节假日
 https://timor.tech/api/holiday/year/2025
+
+可选域名：
+is-working.com
---
+++ src/i18n/languages.ts
@@ -0,0 +1,8 @@
+export const languages = [
+  { code: 'en', name: 'English' },
+  { code: 'es', name: 'Español' },
+  { code: 'hi', name: 'हिंदी' },
+  { code: 'ar', name: 'العربية' },
+  { code: 'zh', name: '中文' },
+  { code: 'ru', name: 'Русский' }
+];
